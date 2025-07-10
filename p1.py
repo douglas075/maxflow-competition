fe70baadbe1f8051e77bc88b8a30d4ae9d10c4c8
douglas <ggg940406@gmail.com>
2025-07-11 03:09:13 +0800

    adjust file structure

--- p1.py
+++ p1.py
@@ -1,5 +1,5 @@
-from maxflow_base import MaxFlowInteractiveBase
-from timer_overlay import TimerOverlay 
+from src.maxflow_base import MaxFlowInteractiveBase
+from src.timer_overlay import TimerOverlay 
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -9,14 +9,14 @@ class MaxFlowInteractive(MaxFlowInteractiveBase):
 
     def build_graph(self):
         G = nx.DiGraph()
-        G.add_edge('S', 'A', capacity=5)
+        G.add_edge('S', 'A', capacity=9)
         G.add_edge('S', 'B', capacity=6)
         G.add_edge('S', 'C', capacity=5)
-        G.add_edge('A', 'D', capacity=9)
-        G.add_edge('B', 'D', capacity=4)
-        G.add_edge('C', 'E', capacity=6)
+        G.add_edge('A', 'D', capacity=5)
+        G.add_edge('B', 'D', capacity=3)
+        G.add_edge('C', 'E', capacity=8)
         G.add_edge('D', 'T', capacity=7)
-        G.add_edge('E', 'T', capacity=7)
+        G.add_edge('E', 'T', capacity=10)
         return G
 
     def fixed_positions(self):
@@ -56,3 +56,21 @@ if __name__ == "__main__":
 
     plt.tight_layout()
     plt.show()
+
+'''
+solution
+
+for each edge, calculate how much maxflow will increase if it's doubled
+in 1st, 2nd, 3rd round, for example:
+
+e2: 3/3/3
+e3: 0/4/0
+e6: 1/1/1
+
+if we choose (6 -> 3 -> 2), the final amount is
+original * 3 + 1 * 3 + 4 * 2 + 3 * 1 = 3 * ori + 14
+
+in the other hand (2 -> 6 -> 3) gives
+original * 3 + 3 * 3 + 1 * 2 + 4 * 1 = 3 * ori + 15
+
+'''
--- src/p2_sol.py
+++ src/p2_sol.py
@@ -1,5 +1,5 @@
-from src.maxflow_base import MaxFlowInteractiveBase
-from src.timer_overlay import TimerOverlay
+from maxflow_base import MaxFlowInteractiveBase
+from timer_overlay import TimerOverlay
 import networkx as nx
 import matplotlib.pyplot as plt
 
@@ -12,15 +12,17 @@ class MaxFlowInteractive(MaxFlowInteractiveBase):
         G = nx.DiGraph()
         # same to p1        
         G.add_edge('S', 'A', capacity=9)
-        G.add_edge('S', 'B', capacity=6)
+        G.add_edge('S', 'B', capacity=4)
+        G.add_edge('S', 'B\'', capacity=2) # for sol
         G.add_edge('S', 'C', capacity=5)
         G.add_edge('A', 'D', capacity=5)
-        G.add_edge('B', 'D', capacity=3)
-        G.add_edge('B', 'E', capacity=4) # new
-        G.add_edge('C', 'E', capacity=6) # new
+        G.add_edge('B\'', 'D', capacity=3) # for sol
+        G.add_edge('B', 'E', capacity=4) 
+        G.add_edge('C', 'E\'', capacity=6) # for sol
         G.add_edge('C', 'F', capacity=8)
         G.add_edge('D', 'T', capacity=7)
-        G.add_edge('E', 'T', capacity=8) # new
+        G.add_edge('E', 'T', capacity=4) 
+        G.add_edge('E\'', 'T', capacity=4) # for sol
         G.add_edge('F', 'T', capacity=10)
 
         return G
@@ -30,9 +32,11 @@ class MaxFlowInteractive(MaxFlowInteractiveBase):
             'S': (0, 2),
             'A': (1, 3),
             'B': (1, 2),
+            'B\'': (1, 2.5),
             'C': (1, 1),
             'D': (2, 3),
             'E': (2, 2),
+            'E\'': (2, 1.5),
             'F': (2, 1),
             'T': (3, 2),
         }
@@ -45,22 +49,22 @@ if __name__ == "__main__":
     mfi1 = MaxFlowInteractive(edge_name_dy=0.12, edge_flow_dy=-0.12, fig=fig, ax=axs[0])
     mfi2 = MaxFlowInteractive(edge_name_dy=0.12, edge_flow_dy=-0.12, fig=fig, ax=axs[1])
 
-    timer = TimerOverlay(fig, duration=120)
+    # timer = TimerOverlay(fig, duration=120)
 
-    def on_click(event):
-        if event.inaxes not in [axs[0], axs[1]]:
-            return
+    # def on_click(event):
+    #     if event.inaxes not in [axs[0], axs[1]]:
+    #         return
 
-        px, py = event.xdata, event.ydata
-        for mfi in [mfi1, mfi2]:
-            for u, v in mfi.G.edges():
-                dist = mfi.point_line_distance(px, py, *mfi.pos[u], *mfi.pos[v])
-                if dist < 0.3:
-                    return  
+    #     px, py = event.xdata, event.ydata
+    #     for mfi in [mfi1, mfi2]:
+    #         for u, v in mfi.G.edges():
+    #             dist = mfi.point_line_distance(px, py, *mfi.pos[u], *mfi.pos[v])
+    #             if dist < 0.3:
+    #                 return  
 
-        timer.toggle()  
+    #     timer.toggle()  
 
-    fig.canvas.mpl_connect("button_press_event", on_click)
+    # fig.canvas.mpl_connect("button_press_event", on_click)
 
     plt.tight_layout()
     plt.show()
